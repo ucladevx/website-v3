07726a5e14cfc893f4d1c726b6c4b043c81a181a
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2025-08-27 00:18:38 -0700

    Route projects and blink pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import "./styles/globals.css";
 import Home  from "./pages/Home";
 import About from "./pages/About";
 import Team   from "./pages/Team"; 
+import Projects from "./pages/Projects"; 
+import Blink from "./projects/Blink";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/"      element={<Home />}  />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/blink" element={<Blink />} />
         <Route path="/about" element={<About />} />
         <Route path="/team"   element={<Team />} />
       </Routes>
