0c235ddb3eac9c9e1e27e7cb04b7cf3703e0b474
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2025-09-02 20:21:42 -0700

    Fix projects navbar

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ProjectsHero from "../components/ProjectsHero";
 import ProjectsGrid from "../components/ProjectsGrid";
-import FooterSection from "../components/FooterSection";
+import NavBar from "../components/NavBar";
 
 export default function Projects() {
   return (
     <div className="projects-page">
+      <header className="project-header">
+        <NavBar />
+      </header>
       <ProjectsHero onLearnMore={() => window.scrollTo({ top: 520, behavior: "smooth" })} />
       <ProjectsGrid />
     </div>
