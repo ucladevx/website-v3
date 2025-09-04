5de1808aea1e64fd425de1aa57c0d8963d1dfbcb
Ishan Royyuru <142559396+ishanroyyuru@users.noreply.github.com>
2025-09-03 19:17:14 -0700

    Added project descriptions

--- src/projects/Blink.jsx
+++ src/projects/Blink.jsx
@@ -16,17 +16,14 @@ export default function Blink() {
         <div className="blink-container">
           <div className="blink-header">
             <h1 className="blink-title">UCLA BLink</h1>
-            <p className="blink-subtitle">one line description of BLink</p>
+            <p className="blink-subtitle">The go-to app for discovering every pop‑up and event happening at UCLA</p>
           </div>
 
           <div className="blink-content">
             <div className="blink-text">
               <h2>What is BLink?</h2>
               <p>
-                Qorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                eu turpis molestie, dictum est a, mattis tellus. Sed dignissim,
-                metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
-                interdum tellus elit sed risus.
+                Founded in 2024, UCLA BLink is a student-run platform that aggregates campus events: pop-ups, student org gatherings, workshops, and more—into one easily accessible spot. Linking you with the best events on campus — BLink and you’ll miss it. 
               </p>
             </div>
             <div className="blink-image">
